02100d55c3dbabea1a118273dcdf15223b9e724f
yak <yak@isaacyakl.com>
2025-07-30 21:09:48 -0700

    Update dotfiles-updater update message

--- .config/yadm/dotfiles-updater.sh
+++ .config/yadm/dotfiles-updater.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if ! yadm pull | grep -q 'Already up to date.'; then 
-    echo '.dotfiles update available'; 
+    echo '.dotfiles update available';
+    echo 'Run "yadm bootstrap" to update your dotfiles';
 else
     echo '.dotfiles up to date';
 fi
